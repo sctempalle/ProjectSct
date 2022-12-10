dbed14b8cf0017607807a606c7e962848af73659
sctempalle <sharathctempalle@gmail.com>
2022-12-10 12:04:11 +0530

    first commit
    first commit

---
+++ src/main/java/com/sharath/projectsct/ProjectSctApplication.java
@@ -0,0 +1,13 @@
+package com.sharath.projectsct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharath/projectsct/ProjectSctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharath.projectsct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
